c3a5d5b525921e6c42a80f2ff681e4df90d8ceea
Tim Braun <164658229+brauntim@users.noreply.github.com>
2024-05-23 18:16:00 +0200

    added errorHandeling

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-#
-#   webscraper for https://www.policija.si/apps/nfl_response_web/seznam.php
-#
-
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -9,21 +5,23 @@ from rdkit import Chem
 from rdkit.Chem import inchi
 import time
 
-#    Diese Klasse handhabt das Abrufen, Parsen und Speichern von Substanzdaten von einer gegebenen URL.
+# Diese Klasse handhabt das Abrufen, Parsen und Speichern von Substanzdaten von einer gegebenen URL.
 class SubstanceExtractor:
 
-#   Initialisiert eine neue Instanz der SubstanceExtractor-Klasse.
+    # Initialisiert eine neue Instanz der SubstanceExtractor-Klasse.
     def __init__(self, url):
         self.url = url
         self.substances = []
 
-#   GET-Anfrage an die Website und gibt HTML-Inhalt zurück
+    # GET-Anfrage an die Website und gibt HTML-Inhalt zurück
     def fetch_data(self):
         response = requests.get(self.url)
         response.raise_for_status()
         return response.text
     
+
     def get_inchi_from_inchikey(self, inchi_key):
+        time.sleep(1)
         inchi_url = f'https://cactus.nci.nih.gov/chemical/structure/{inchi_key}/inchi'
         response = requests.get(inchi_url)
         if response.status_code == 200:
@@ -36,79 +34,86 @@ class SubstanceExtractor:
             return Chem.MolToSmiles(mol)
         return None
 
+    # Diese Funktion parst eine einzelne Zeile und gibt die Substanzdaten zurück.
+    def parse_row(self, row):
+        columns = row.find_all('td')
+        if len(columns) < 17:  # Sicherstellen, dass die Zeile genügend Spalten hat
+            return None
 
-#   Parst den HTML-Inhalt und extrahiert die Substanzdaten
+        # Extrahieren der Daten aus den entsprechenden Spalten
+        category = columns[0].text.strip()
+        name = columns[1].text.strip()
+        iupac_name = columns[3].text.strip()
+        formular = columns[5].text.strip()
+        molecular_mass = columns[6].text.strip()
+        inchi_key = columns[12].text.strip()
+        date_of_entry = columns[15].text.strip()
+        report_updated = columns[16].text.strip()
+
+
+        inchi = self.get_inchi_from_inchikey(inchi_key)
+        
+        smiles = self.convert_inchi_to_smiles(inchi)
+
+        # Bestimmen des Datums der letzten Änderung
+        if report_updated:
+            last_changed_at = report_updated
+        else:
+            last_changed_at = date_of_entry
+
+        # Überprüfen, ob die Substanz gelöscht wurde
+        deleted = '<strike>' in name
+
+        # Erstellen eines Dictionaries mit den extrahierten Daten
+        substance_data = {
+            "smiles": smiles,
+            "names": name,
+            "iupac_name": iupac_name,
+            "formular": formular,
+            "molecular_mass": molecular_mass,
+            "Inchi": inchi,
+            "InchiKey": inchi_key,
+            "cas_num": "",  # Wird aus anderen Informationen abgeleitet
+            "category": category,
+            "source_name": "Policija",
+            "source_url": self.url,
+            "valid": None,  # Nicht genügend Informationen, um dies zu bestimmen
+            "deleted": deleted,
+            "last_changed_at": last_changed_at,
+            "version": "1.0",
+            "details": {
+                # Details unseres Teams
+            }
+        }
+        return substance_data
+
+    # Parst den HTML-Inhalt und extrahiert die Substanzdaten
     def parse_html(self, html):
         soup = BeautifulSoup(html, 'html.parser')
         for row in soup.select('table tr'):
-            columns = row.find_all('td')
-            if len(columns) < 17:  # Sicherstellen, dass die Zeile genügend Spalten hat
-                continue
-
-            # Extrahieren der Daten aus den entsprechenden Spalten
-            category = columns[0].text.strip()
-            name = columns[1].text.strip()
-            # smiles = columns[2].text.strip()
-            iupac_name = columns[3].text.strip()
-            formular = columns[5].text.strip()
-            molecular_mass = columns[6].text.strip()
-            inchi_key = columns[12].text.strip()
-            date_of_entry = columns[15].text.strip()
-            report_updated = columns[16].text.strip()
-            # details =
-
-           # inchi = self.get_inchi_from_inchikey(inchi_key)
-           # smiles = self.convert_inchi_to_smiles(inchi) 
-
-            # Bestimmen des Datums der letzten Änderung
-            if report_updated:
-                last_changed_at = report_updated
-            else:
-                last_changed_at = date_of_entry
+            try:
+                substance_data = self.parse_row(row)
+                if substance_data:
+                    self.substances.append(substance_data)
+            except Exception as e:
+                print(f"Error parsing row: {row}" )
                 
-            # Überprüfen, ob die Substanz gelöscht wurde
-            deleted = '<strike>' in name
-
-            # Erstellen eines Dictionaries mit den extrahierten Daten
-            substance_data = {
-                "smiles": "",
-                "names": name,
-                "iupac_name": iupac_name,
-                "formular": formular,
-                "molecular_mass": molecular_mass,
-                "Inchi": "",  
-                "InchiKey": inchi_key,
-                "cas_num": "",  # Wird aus anderen Informationen abgeleitet
-                "category": category,
-                "source_name": "Policija",
-                "source_url": self.url,
-                "valid": None,  # Nicht genügend Informationen, um dies zu bestimmen
-                "deleted": deleted,
-                "last_changed_at": last_changed_at,
-                "version": "1.0",
-                "details": {
-                   # Details unseres Teams
-                }
-            }
-
-            # Hinzufügen der Substanzdaten zur Liste
-            self.substances.append(substance_data)
 
 
-#   Speichert die extrahierten Substanzdaten in einer JSON-Datei.
+    # Speichert die extrahierten Substanzdaten in einer JSON-Datei.
     def save_to_json(self, filename):
         with open(filename, 'w') as json_file:
             json.dump(self.substances, json_file, indent=4)
 
-#   Führt den gesamten Extraktionsprozess aus.
+    # Führt den gesamten Extraktionsprozess aus.
     def run(self):
         html = self.fetch_data()
         self.parse_html(html)
         self.save_to_json('substances.json')
 
-    
+
 if __name__ == "__main__":
     url = "https://www.policija.si/apps/nfl_response_web/seznam.php"
     extractor = SubstanceExtractor(url)
     extractor.run()
-    print("Datenextraktion abgeschlossen. Die Ergebnisse sind in substances.json gespeichert.")
+    print("Datenextraktion abgeschlossen. Die Ergebnisse sind in substancesTest.json gespeichert.")
