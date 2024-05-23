35e3bc9fcae0501102b11219534c30e6e8bcba03
Tim Braun <164658229+brauntim@users.noreply.github.com>
2024-05-23 22:34:29 +0200

    finished part 1 of project (complete json)
    added following:
    
    - seperated function "parse_html" into "parse_html" and into "parse_row", so it was easier to catch an error, 
    --> and code is easier to understand:
    -----> "parse_row" is for each row in https://www.policija.si/apps/nfl_response_web/seznam.php
    
    - API (https://opsin.ch.cam.ac.uk/) to convert IUPAC to SMILES & INCHI
    - function for smiles to canonical smiles
    
    - some try and catch to see where problems occur
    
    - in JSON at "iupac_name" there was "\u2010" instead "-" had to get rid of it with a function, that replaces "\u2010" with "-"

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+#
+#   webscraper for https://www.policija.si/apps/nfl_response_web/seznam.php
+#
+
 import requests
 from bs4 import BeautifulSoup
 import json
 from rdkit import Chem
-from rdkit.Chem import inchi
-import time
+from rdkit.Chem import Descriptors
+# import time
 
 # Diese Klasse handhabt das Abrufen, Parsen und Speichern von Substanzdaten von einer gegebenen URL.
 class SubstanceExtractor:
@@ -19,22 +23,26 @@ class SubstanceExtractor:
         response.raise_for_status()
         return response.text
     
+    def convert_to_canonical_smiles(self, smiles):
+        molecule = Chem.MolFromSmiles(smiles)
+        canonical_smiles = Chem.MolToSmiles(molecule, canonical=True)
+        return canonical_smiles
 
-    def get_inchi_from_inchikey(self, inchi_key):
-        time.sleep(1)
-        inchi_url = f'https://cactus.nci.nih.gov/chemical/structure/{inchi_key}/inchi'
-        response = requests.get(inchi_url)
+    # Funktion zum Abrufen von StdInChI und SMILES von OPSIN-API
+    def fetch_opsin_data(self, iupac_name):
+        opsin_url = f"https://opsin.ch.cam.ac.uk/opsin/{iupac_name}.json"
+        response = requests.get(opsin_url)
         if response.status_code == 200:
-            return response.text.strip()
-        return None
+            data = response.json()
+            smiles_str = data.get('smiles', '')
+            inchi_str = data.get('stdinchi', '')
+            # Konvertieren des SMILES-Strings in einen kanonischen SMILES-String
+            smiles_str = self.convert_to_canonical_smiles(smiles_str)
+            return smiles_str, inchi_str
+        else:
+            return "", ""
 
-    def convert_inchi_to_smiles(self, inchi):
-        mol = Chem.MolFromInchi(inchi)
-        if mol:
-            return Chem.MolToSmiles(mol)
-        return None
-
-    # Diese Funktion parst eine einzelne Zeile und gibt die Substanzdaten zurück.
+    # Funktion parst eine einzelne Zeile und gibt die Substanzdaten zurück.
     def parse_row(self, row):
         columns = row.find_all('td')
         if len(columns) < 17:  # Sicherstellen, dass die Zeile genügend Spalten hat
@@ -46,14 +54,9 @@ class SubstanceExtractor:
         iupac_name = columns[3].text.strip()
         formular = columns[5].text.strip()
         molecular_mass = columns[6].text.strip()
-        inchi_key = columns[12].text.strip()
+        std_inchi_key = columns[12].text.strip()
         date_of_entry = columns[15].text.strip()
-        report_updated = columns[16].text.strip()
-
-
-        inchi = self.get_inchi_from_inchikey(inchi_key)
-        
-        smiles = self.convert_inchi_to_smiles(inchi)
+        report_updated = columns[16].text.strip() 
 
         # Bestimmen des Datums der letzten Änderung
         if report_updated:
@@ -64,6 +67,16 @@ class SubstanceExtractor:
         # Überprüfen, ob die Substanz gelöscht wurde
         deleted = '<strike>' in name
 
+        # Abrufen von SMILES und InChI über OPSIN-API mittels IUPAC
+        try:
+            smiles, std_inchi = self.fetch_opsin_data(iupac_name)
+            # print(smiles)
+            # print(std_inchi)
+            # time.sleep(10)
+        except Exception as e:
+            print(f"Error fetching data for IUPAC name {iupac_name}: {e}")
+            smiles, std_inchi = "", ""
+
         # Erstellen eines Dictionaries mit den extrahierten Daten
         substance_data = {
             "smiles": smiles,
@@ -71,8 +84,8 @@ class SubstanceExtractor:
             "iupac_name": iupac_name,
             "formular": formular,
             "molecular_mass": molecular_mass,
-            "Inchi": inchi,
-            "InchiKey": inchi_key,
+            "Inchi": std_inchi,
+            "InchiKey": std_inchi_key,
             "cas_num": "",  # Wird aus anderen Informationen abgeleitet
             "category": category,
             "source_name": "Policija",
@@ -90,30 +103,39 @@ class SubstanceExtractor:
     # Parst den HTML-Inhalt und extrahiert die Substanzdaten
     def parse_html(self, html):
         soup = BeautifulSoup(html, 'html.parser')
-        for row in soup.select('table tr'):
+        for index,row in enumerate(soup.select('table tr'), start=1):
             try:
                 substance_data = self.parse_row(row)
                 if substance_data:
                     self.substances.append(substance_data)
             except Exception as e:
-                print(f"Error parsing row: {row}" )
-                
+                print(f"Error parsing row: {index}" )
 
+    # Funktion zum Ersetzen von "\u2010" durch "-" in JSON Datei
+    def replace_unicode_hyphen(self, data):
+        if isinstance(data, str):
+            return data.replace("\\u2010", "-")
+        elif isinstance(data, list):
+            return [self.replace_unicode_hyphen(item) for item in data]
+        elif isinstance(data, dict):
+            return {key: self.replace_unicode_hyphen(value) for key, value in data.items()}
+        else:
+            return data
 
     # Speichert die extrahierten Substanzdaten in einer JSON-Datei.
     def save_to_json(self, filename):
-        with open(filename, 'w') as json_file:
-            json.dump(self.substances, json_file, indent=4)
+        updated_substances = self.replace_unicode_hyphen(self.substances)
+        with open(filename, 'w', encoding='utf-8') as json_file:
+            json.dump(updated_substances, json_file, indent=4)
 
     # Führt den gesamten Extraktionsprozess aus.
     def run(self):
         html = self.fetch_data()
         self.parse_html(html)
-        self.save_to_json('substances.json')
-
+        self.save_to_json('substancesV4.json')
 
 if __name__ == "__main__":
     url = "https://www.policija.si/apps/nfl_response_web/seznam.php"
     extractor = SubstanceExtractor(url)
     extractor.run()
-    print("Datenextraktion abgeschlossen. Die Ergebnisse sind in substancesTest.json gespeichert.")
+    print("Datenextraktion abgeschlossen. Die Ergebnisse sind in substancesV4.json gespeichert.")
