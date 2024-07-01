64929c961573ccf6d70bc331a34cfee2108b9684
Tim Braun <164658229+brauntim@users.noreply.github.com>
2024-07-01 22:53:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 import json
 from rdkit import Chem
 from rdkit.Chem import Descriptors
-
 import logging
 import os
 
@@ -17,16 +16,20 @@ os.makedirs('jsons', exist_ok=True)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+# Log-Format für Dateilogs
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%d-%m-%Y %H:%M:%S")
 
+# Dateilogs konfigurieren
 file_logger = logging.FileHandler('logs/scraper.log')
 file_logger.setLevel(logging.DEBUG)
 file_logger.setFormatter(formatter)
 
+# Konsolenlogs konfigurieren
 console_logger = logging.StreamHandler()
 console_logger.setLevel(logging.INFO)
 console_logger.setFormatter(formatter)
 
+# Füge beide Logger hinzu
 logger.addHandler(file_logger)
 logger.addHandler(console_logger)
 
@@ -36,17 +39,20 @@ class SubstanceExtractor:
         self.url = url
         self.substances = []
 
+    # HTML-Daten von der URL abrufen
     def fetch_data(self):
         logger.info("Scraping started")
         response = requests.get(self.url)
-        response.raise_for_status()
+        response.raise_for_status()  # Fehler auslösen, wenn die Anfrage fehlschlägt
         return response.text
 
+    # Konvertiere SMILES in kanonisches SMILES-Format
     def convert_to_canonical_smiles(self, smiles):
         molecule = Chem.MolFromSmiles(smiles)
         canonical_smiles = Chem.MolToSmiles(molecule, canonical=True)
         return canonical_smiles
 
+    # Daten von OPSIN-Datenbank abrufen
     def fetch_opsin_data(self, iupac_name):
         opsin_url = f"https://opsin.ch.cam.ac.uk/opsin/{iupac_name}.json"
         response = requests.get(opsin_url)
@@ -59,11 +65,13 @@ class SubstanceExtractor:
         else:
             return "", ""
 
+    # Eine einzelne Zeile der HTML-Tabelle parsen
     def parse_row(self, row):
         columns = row.find_all('td')
-        if len(columns) < 17:
+        if len(columns) < 17:  # Überprüfe, ob die Zeile genügend Spalten hat
             return None
 
+        # Extrahiere Daten aus den Tabellenzellen
         category = columns[0].text.strip()
         name = columns[1].text.strip()
         iupac_name = columns[3].text.strip()
@@ -74,14 +82,16 @@ class SubstanceExtractor:
         report_updated = columns[16].text.strip()
 
         last_changed_at = report_updated if report_updated else date_of_entry
-        deleted = '<strike>' in name
+        deleted = '<strike>' in name  # Überprüfe, ob die Substanz als gelöscht markiert ist
 
         try:
+            # Versuche, SMILES und InChI von OPSIN zu holen
             smiles, std_inchi = self.fetch_opsin_data(iupac_name)
         except Exception as e:
             logger.error(f"Error fetching data for IUPAC name {iupac_name}: {e}")
             smiles, std_inchi = "", ""
 
+        # Erstelle ein Dictionary mit den extrahierten und abgerufenen Daten
         substance_data = {
             "version": "1.0",
             "smiles": smiles,
@@ -104,6 +114,7 @@ class SubstanceExtractor:
         }
         return substance_data
 
+    # Den gesamten HTML-Inhalt parsen
     def parse_html(self, html):
         soup = BeautifulSoup(html, 'html.parser')
         for index, row in enumerate(soup.select('table tr'), start=1):
@@ -116,10 +127,11 @@ class SubstanceExtractor:
                 print(f"Error parsing row: {index}")
                 logger.error(f"Fehler beim  Scrapen: {index}")
 
+    # Unicode-Zeichen ersetzen
     def replace_unicode_characters(self, data):
         replacements = {
-            "\u2010": "-",
-            "\u200b": "",
+            "\u2010": "-",  # Hyphen
+            "\u200b": "",  # Zero Width Space
         }
         if isinstance(data, str):
             for key, value in replacements.items():
@@ -134,6 +146,7 @@ class SubstanceExtractor:
         else:
             return data
 
+    # Daten validieren
     def validate_data(self):
         for substance in self.substances:
             smiles = substance.get("smiles", "")
@@ -155,6 +168,7 @@ class SubstanceExtractor:
             else:
                 substance["validated"] = None
 
+    # Daten in eine JSON-Datei speichern
     def save_to_json(self, filename):
         updated_substances = self.replace_unicode_characters(self.substances)
         with open(f"jsons/{filename}", 'w', encoding='utf-8') as json_file:
@@ -162,19 +176,19 @@ class SubstanceExtractor:
         logger.info(f"Saving substances to {filename}")
         logger.info("Scraping finished")
 
+    # Scraper ausführen
     def run(self, filename):
         html = self.fetch_data()
         self.parse_html(html)
         self.validate_data()
         self.save_to_json(filename)
 
-
+# Scraper starten
 def start_scraping(filename):
     url = "https://www.policija.si/apps/nfl_response_web/seznam.php"
     extractor = SubstanceExtractor(url)
     extractor.run(filename)
     print(f"Datenextraktion abgeschlossen. Die Ergebnisse sind in jsons/{filename} gespeichert.")
-
 
 if __name__ == "__main__":
     start_scraping("Tim_Jonas_Policija.json")
